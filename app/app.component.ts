9969f400d34d4fe5517a2911f4fa8d2193c9d77f
Alexander Jacks and Julia Dickey <julia@>
2016-08-09 09:54:43 -0700

    adds child component method and custom event emitter

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,59 @@
-import { Component} from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  template: `
+  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
+      {{ currentTask.description }}
+  </h3>
+  `
+})
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log(clickedTask, "from child");
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
 
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
   <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
+    <h1>To-Do List</h1>
+    <task-list
+      [taskList]="tasks"
+      (onTaskSelect)="taskWasSelected($event)">
+    </task-list>
   </div>
   `
 })
 
 export class AppComponent {
+  public tasks: Task[]; // Task[] (or Array<Task>) identifies tasks as an array of Task objects
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn kung fu.", 1),
+      new Task("Do interesting stuff.", 3),
+      new Task("Write about interesting stuff.", 2)
+    ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log(clickedTask, "from parent");
+  }
+}
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
 }
